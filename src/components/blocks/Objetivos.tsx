6cae10748c74d38a6f4dd532f8537d501427dca2
Rudy Anconi <rudyanconi@gmail.com>
2024-04-11 10:13:24 -0300

    objetivos

--- src/components/blocks/Objetivos.tsx
+++ src/components/blocks/Objetivos.tsx
@@ -9,34 +9,39 @@ const Objetivos = ({ slice }: BlocoProps) => {
                 <div className="flex-1 bg-background"></div>
                 <div className="flex-1 bg-foreground"></div>
             </div>
-            <div className="container m-auto bg-background text-foreground rounded-[5rem] rounded-tl-none py-16 px-6 md:p-16 relative z-1">
-                <div className="flex items-center gap-3 justify-end">
-                    <PrismicNextImage
-                        alt=""
-                        width={40}
-                        height={40}
-                        className="*:fill-white"
-                        field={slice.primary.icone}
-                    />
-                    <h1
-                        className="text-4xl relative
-                after:content[''] after:absolute after:-bottom-3 after:left-0 after:h-2 after:bg-foreground after:w-24
-                
-                "
-                    >
-                        {slice.primary.titulo}
-                    </h1>
-                </div>
-                <div className="grid items-center gap-3 justify-end  my-16">
-                    {slice.items.map((item, k) => (
-                        <div
-                            key={k}
-                            className="text-2xl md:text-3xl lg:text-4xl text-right pr-7 relative
-                            after:content[''] after:absolute after:right-0 after:top-3 after:h-3 after:bg-foreground after:w-3"
+            <div className="container m-auto bg-background text-foreground rounded-[5rem] rounded-tl-none pt-80 px-6 md:p-16 md:pt-0 md:pb-0 relative z-1">
+                <PrismicNextImage
+                    alt=""
+                    className="[ md:-ml-40 md:scale-75 md:-mb-24 ] 
+                    [ lg:-ml-12 lg:scale-100 ]"
+                    field={slice.primary.imagem}
+                />
+                <div className="absolute md:right-14 right-12 top-12 md:top-1/2 md:-translate-y-1/2">
+                    <div className="flex items-center gap-3 justify-end">
+                        <PrismicNextImage
+                            alt=""
+                            width={40}
+                            height={40}
+                            field={slice.primary.icone}
+                        />
+                        <h1
+                            className="text-4xl relative
+                after:content[''] after:absolute after:-bottom-3 after:left-0 after:h-2 after:bg-foreground after:w-24"
                         >
-                            <PrismicRichText field={item.conteudo} />
-                        </div>
-                    ))}
+                            {slice.primary.titulo}
+                        </h1>
+                    </div>
+                    <div className="grid items-center gap-3 justify-end my-16">
+                        {slice.items.map((item, k) => (
+                            <div
+                                key={k}
+                                className="text-2xl md:text-2xl lg:text-4xl text-right pr-7 relative
+                            after:content[''] after:absolute after:right-0 after:top-3 after:h-3 after:bg-foreground after:w-3"
+                            >
+                                <PrismicRichText field={item.conteudo} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
