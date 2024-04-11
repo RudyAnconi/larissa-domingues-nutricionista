5faeb235e7e72a79956e85010aa551cefb10e68f
Rudy Anconi <rudyanconi@gmail.com>
2024-04-10 23:35:10 -0300

    Objetivos, falta a foto

--- src/components/blocks/Objetivos.tsx
+++ src/components/blocks/Objetivos.tsx
@@ -1,4 +1,6 @@
 import { BlocoProps } from "@/slices/Bloco";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText } from "@prismicio/react";
 
 const Objetivos = ({ slice }: BlocoProps) => {
     return (
@@ -7,9 +9,35 @@ const Objetivos = ({ slice }: BlocoProps) => {
                 <div className="flex-1 bg-background"></div>
                 <div className="flex-1 bg-foreground"></div>
             </div>
-            <div className="container m-auto text-foreground bg-background rounded-[5rem] p-16 relative z-1">
-                <h1 className="text-4xl font-bold">{slice.primary.titulo}</h1>
-                <p>{slice.primary.codigo}</p>
+            <div className="container m-auto bg-background text-foreground rounded-[5rem] rounded-tl-none py-16 px-6 md:p-16 relative z-1">
+                <div className="flex items-center gap-3 justify-end">
+                    <PrismicNextImage
+                        alt=""
+                        width={40}
+                        height={40}
+                        className="*:fill-white"
+                        field={slice.primary.icone}
+                    />
+                    <h1
+                        className="text-4xl relative
+                after:content[''] after:absolute after:-bottom-3 after:left-0 after:h-2 after:bg-foreground after:w-24
+                
+                "
+                    >
+                        {slice.primary.titulo}
+                    </h1>
+                </div>
+                <div className="grid items-center gap-3 justify-end  my-16">
+                    {slice.items.map((item, k) => (
+                        <div
+                            key={k}
+                            className="text-2xl md:text-3xl lg:text-4xl text-right pr-7 relative
+                            after:content[''] after:absolute after:right-0 after:top-3 after:h-3 after:bg-foreground after:w-3"
+                        >
+                            <PrismicRichText field={item.conteudo} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
